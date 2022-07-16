0f6583969939ae2256953e0263b9dadcc0e9e71e
Alexandre J. Martins <alexandre@ajmartins.com.br>
2022-07-15 23:23:28 -0300

    Largest palindrome product solved

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 
 1. [Multiples of 3 or 5](challenge001.php)
 2. [Even Fibonacci numbers](challenge002.php)
+
+3. [Largest palindrome product](challenge004.php)
---
+++ challenge004.php
@@ -0,0 +1,23 @@
+<?php
+
+# Problem 4
+
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+$num1 = 999;
+$num2 = 999;
+
+function largestPalindrome($num1, $num2) {
+    $result = $num1 * $num2;
+    if ($result == strrev($result)) {
+        return $result;
+    }
+    return largestPalindrome($num1, $num2 - 1);
+}
+
+for ($num1; $num1 > 0; $num1--) {
+    $palidromes[] = largestPalindrome($num1, $num2);
+}
+
+echo (max($palidromes));
