79cb0d1c5649fe7e7538b1c6a942a3f578ec9d6e
Alexandre J. Martins <alexandre@ajmartins.com.br>
2022-07-17 13:23:09 -0300

    Improve prologue

--- challenge004.php
+++ challenge004.php
@@ -1,9 +1,11 @@
 <?php
-
-# Problem 4
-
-# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-# Find the largest palindrome made from the product of two 3-digit numbers.
+/**
+ * Largest palindrome product
+ *
+ * Problem: A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ * 
+ */
 
 $num1 = 999;
 $num2 = 999;
